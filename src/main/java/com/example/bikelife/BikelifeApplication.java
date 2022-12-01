4bcac8851b97727543a31c7745795db244c99f14
woong <woonghk1130@gmail.com>
2022-12-01 22:36:04 +0900

    도전2

--- src/main/java/com/example/bikelife/BikelifeApplication.java
+++ src/main/java/com/example/bikelife/BikelifeApplication.java
@@ -2,11 +2,14 @@ package com.example.bikelife;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class BikelifeApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BikelifeApplication.class, args);
     }
 
