dd51689f897c58b63b23aa21a9a540bf95b5b0a0
woong <woonghk1130@gmail.com>
2022-12-01 21:42:20 +0900

    Initial commit

---
+++ src/main/java/com/example/bikelife/BikelifeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bikelife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikelifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikelifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bikelife/BikelifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bikelife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikelifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
