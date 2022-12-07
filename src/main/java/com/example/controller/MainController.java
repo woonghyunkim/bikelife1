32b5e40b985dfbfe02b547e619b301f8fa9660fc
woong <woonghk1130@gmail.com>
2022-12-07 21:51:44 +0900

    푸쉬

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -34,6 +34,31 @@ public class MainController {
         return "views/user/signup";
     }
 
+    //라이딩 화면 이동
+    @GetMapping("riding")
+    public String riding() {
+        return "views/user/riding";
+    }
+
+    //플랙스 화면 이동
+    @GetMapping("flex")
+    public String flex() {
+        return "views/user/flex";
+    }
+
+    //커뮤니티 화면 이동
+    @GetMapping("community")
+    public String community() {
+        return "views/user/community";
+    }
+
+    //어바웃 화면 이동
+    @GetMapping("about")
+    public String about() {
+        return "views/user/about";
+    }
+
+
 
 
 
